277bbf75f552e8599466b6c01daf9968ac398654
Santiago <santigp258@gmail.com>
2021-10-29 11:57:17 -0500

    example

--- example/pages/nested/deep.js
+++ example/pages/nested/deep.js
@@ -5,7 +5,6 @@ import Breadcrumbs from 'nextjs-breadcrumbs';
 const App = () => {
   return (
     <Breadcrumbs
-      useDefaultStyle={true}
       transformLabel={(title) => title}
       SeparatorComponent={() => (
         <svg
